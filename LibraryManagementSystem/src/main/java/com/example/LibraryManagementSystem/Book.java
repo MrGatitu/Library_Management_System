fe4ba17b8fcbea9b633406aecb9d4ea36074584c
MrGatitu <mrgatituh@gmail.com>
2024-08-09 17:00:18 +0300

    Added Getters and Setters to Adhere to OOP guidlines and POST method works

--- LibraryManagementSystem/src/main/java/com/example/LibraryManagementSystem/Book.java
+++ LibraryManagementSystem/src/main/java/com/example/LibraryManagementSystem/Book.java
@@ -13,11 +13,11 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Integer id;
+   private Long id;
 
-    String author;
-    String title;
-    int year;
+   private String author;
+   private String title;
+   private int year;
 
     Book(){}
 
@@ -27,12 +27,36 @@ public class Book {
         this.year = year;
     }
 
-    @Override
-    public String toString(){
-        return String.format(
-            "Book[author = "+ author +", title = "+ title +", year = "+ year +" ]"
-        );
+    public String getAuthor() {
+        return author;
     }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    // @Override
+    // public String toString(){
+    //     return String.format(
+    //         "Book[author = "+ author +", title = "+ title +", year = "+ year +" ]"
+    //     );
+    // }
 
     // public static void main(String[] args) {
     //    Book newBook = new Book("Ian", "Letter to Her", 2024);
